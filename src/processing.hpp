0d6015004ac65fe9f8c6ec8c6dab73e9b617641e
OkayMusic <richard.brearton@physics.ox.ac.uk>
2018-09-21 15:24:21 +0100

    Removed old unused files

--- src/processing.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace rrec
-{
-cv::Mat contrast(cv::Mat image);
-cv::Mat fourierTransform(cv::Mat image);
-// void cluster(cv::Mat image, cv::Mat brightnessScale);
-cv::Mat detectSignal(cv::Mat image, cv::Mat brightnessScale);
-} // namespace rrec
